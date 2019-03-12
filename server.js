7036bf9cc7f5c193bf40e4bd728038d046156282
Dezh <dezhaycaraway@gmail.com>
2019-03-12 16:28:38 -0700

    changed js file to test ref

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
-var express = require('express');
-const app = express();
-var path = require('path');
+'use strict';
 
-app.use(express.static(__dirname + '/public'));
+// We’re setting up an extremely simple server here.
+const http = require('http');
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname + '/public/index.html'));
-});
+// These could (should) be set as env vars.
+const port = process.env.PORT || 5000;
+const host = process.env.HOST || 'localhost';
 
-app.listen(3000, () => console.log('Waiting app longs for you on port 3000!'));
+// No matter what hits the server, we send the same thing.
+http.createServer((req, res) => {
+
+  // Tell the browser what’s coming.
+  res.writeHead(200, {
+    'Content-Type': 'text/html; charset=utf-8',
+  });
+
+  // Send a simple message in HTML.
+  res.write('<h1>I’m a Node app!</h1>');
+  res.write('<p>And I’m <em>sooooo</em> secure.</p>');
+  res.end();
+}).listen(port, host);
+
+// This message prints in the console when the app starts.
+console.log(`App running at http://${host}:${port}`);
