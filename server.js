05fe43d1e5da5e51cfb04325a5ad862d65a59461
Dezh <dezhaycaraway@gmail.com>
2019-03-12 17:41:52 -0700

    working server finally

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
-'use strict';
+var express = require('express');
+const app = express();
 
-// We’re setting up an extremely simple server here.
-const http = require('http');
+const PORT = process.env.PORT = 5000;
 
-// These could (should) be set as env vars.
-const port = process.env.PORT || 5000;
-const host = process.env.HOST || 'localhost';
+app.use(express.static('public'));
 
-// No matter what hits the server, we send the same thing.
-http.createServer((req, res) => {
+app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
-  // Tell the browser what’s coming.
-  res.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf-8',
-  });
-
-  // Send a simple message in HTML.
-  res.write('<h1>I’m a Node app!</h1>');
-  res.write('<p>And I’m <em>sooooo</em> secure.</p>');
-  res.end();
-}).listen(port, host);
-
-// This message prints in the console when the app starts.
-console.log(`App running at http://${host}:${port}`);
+app.listen(PORT, () => {
+  console.log('Listening at:',PORT);
+});
